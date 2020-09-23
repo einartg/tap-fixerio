97e959b1eb66491f0c15f1c08d25f8ee37241886
Einar Gudmundsson <einartg@users.noreply.github.com>
2020-09-23 21:45:51 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(name='tap-fixerio',
       classifiers=['Programming Language :: Python :: 3 :: Only'],
       py_modules=['tap_fixerio'],
       install_requires=['singer-python>=0.1.0',
-                        'backoff==1.3.2',
+                        'backoff==1.8.0',
                         'requests==2.20.0'],
       entry_points='''
           [console_scripts]
